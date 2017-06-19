8acbf90587ed23e02289f19247a474ce8a3ed859
Itai Gershtansky <itai.gershtansky@gmail.com>
2017-06-18 20:19:53 -0700

    Express TransactionInMonthCondition with existing code.
    TransactionInMonthCondition now extends TransactionInInclusivePeriodCondition.
    Refactored tests, and added to test new potential edge cases.

--- src/main/java/org/itai/expenses/core/condition/TransactionInMonthCondition.java
+++ src/main/java/org/itai/expenses/core/condition/TransactionInMonthCondition.java
@@ -1,22 +1,11 @@
 package org.itai.expenses.core.condition;
 
-import org.itai.expenses.core.Transaction;
 import org.joda.time.DateTime;
 
-public class TransactionInMonthCondition implements TransactionCondition {
-
-   private int year;
-   private int month;
+public class TransactionInMonthCondition extends TransactionInInclusivePeriodCondition {
 
    public TransactionInMonthCondition(int year, int month) {
-      this.year = year;
-      this.month = month;
-   }
-
-   @Override
-   public boolean isMatch(Transaction transaction) {
-      DateTime time = transaction.getTime();
-      return time.getYear() == this.year
-         && time.getMonthOfYear() == this.month;
+      super(new DateTime(year, month, 1, 0, 0),
+            new DateTime(year, month, 1, 0, 0).plusMonths(1).minusDays(1));
    }
 }
--- src/test/java/org/itai/expenses_core/test/BasicExpenseBookTest.java
+++ src/test/java/org/itai/expenses_core/test/BasicExpenseBookTest.java
@@ -75,60 +75,74 @@ public class BasicExpenseBookTest {
 
    @Test
    public void getAllTransactionsFromMonth() {
-      Transaction t1 = new Expense(450, "Lunch", "Food", new DateTime(2017, 2, 3, 0, 0));
-      Transaction t2 = new Expense(120, "Massage", "Health", new DateTime(2017, 2, 6, 0, 0));
-      Transaction t3 = new Expense(100, "Trader Joe's", "Food at home", new DateTime(2017, 3, 4, 0, 0));
-      Transaction t4 = new Income(4000, "Salary", "Salary", new DateTime(2017, 2, 1, 0, 0));
-      Transaction t5 = new Income(4000, "Salary", "Salary", new DateTime(2017, 3, 1, 0, 0));
+      Transaction[] transactions = new Transaction[]
+         {new Expense(450, "Lunch", "Food",        new DateTime(2017, 1, 31, 0, 0))  //0
+         ,new Expense(450, "Lunch", "Food",        new DateTime(2017, 2, 1, 0, 0))   //1
+         ,new Expense(450, "Lunch", "Food",        new DateTime(2017, 2, 3, 0, 0))   //2
+         ,new Expense(120, "Massage", "Health",    new DateTime(2017, 2, 6, 0, 0))   //3
+         ,new Expense(120, "Massage", "Health",    new DateTime(2017, 3, 1, 0, 0))   //4
+         ,new Expense(100, "Trader Joe's", "Food", new DateTime(2017, 3, 4, 0, 0))   //5
+         ,new Income(4000, "Salary", "Salary",     new DateTime(2017, 3, 15, 0, 0))  //6
+         ,new Income(4000, "Salary", "Salary",     new DateTime(2017, 3, 31, 0, 0))  //7
+         ,new Income(4000, "Salary", "Salary",     new DateTime(2017, 4, 1, 0, 0))   //8
+         ,new Income(4000, "Salary", "Salary",     new DateTime(2017,12, 1, 0, 0))   //9
+         ,new Expense(100, "Lunch", "Food",        new DateTime(2017,12, 15, 0, 0))  //10
+         ,new Expense(120, "Massage", "Health",    new DateTime(2017,12, 31, 0, 0))  //11
+         ,new Expense(100, "Plush Toy", "Other",   new DateTime(2018, 1, 1, 0, 0))   //12
+         };
       ExpenseBook book = new ExpenseBook();
-      book.addTransaction(t1);
-      book.addTransaction(t2);
-      book.addTransaction(t3);
-      book.addTransaction(t4);
-      book.addTransaction(t5);
+      for (Transaction t : transactions) {
+         book.addTransaction(t);
+      }
 
       TransactionCondition conditionInFebruary = new TransactionInMonthCondition(2017, 2);
       Collection<Transaction> inFebruary = book.getTransactionsForCondition(conditionInFebruary);
       assertEquals(3, inFebruary.size());
-      assertTrue(inFebruary.contains(t1));
-      assertTrue(inFebruary.contains(t2));
-      assertTrue(inFebruary.contains(t4));
+      assert (inFebruary.contains(transactions[1]));
+      assert (inFebruary.contains(transactions[2]));
+      assert (inFebruary.contains(transactions[3]));
 
       TransactionCondition conditionInMarch = new TransactionInMonthCondition(2017, 3);
       Collection<Transaction> inMarch = book.getTransactionsForCondition(conditionInMarch);
-      assertEquals(2, inMarch.size());
-      assertTrue(inMarch.contains(t3));
-      assertTrue(inMarch.contains(t5));
+      assertEquals(4, inMarch.size());
+      assert (inMarch.contains(transactions[4]));
+      assert (inMarch.contains(transactions[5]));
+      assert (inMarch.contains(transactions[6]));
+      assert (inMarch.contains(transactions[7]));
+
+      TransactionCondition conditionInDec = new TransactionInMonthCondition(2017, 12);
+      Collection<Transaction> inDec = book.getTransactionsForCondition(conditionInDec);
+      assertEquals(3, inDec.size());
+      assert (inDec.contains(transactions[9]));
+      assert (inDec.contains(transactions[10]));
+      assert (inDec.contains(transactions[11]));
    }
 
    @Test
    public void getAllExpensesInPeriod() {
-      Transaction t1 = new Income(4000, "Salary", "Salary",             new DateTime(2017, 2, 1, 0, 0));
-      Transaction t2 = new Expense(450, "Lunch", "Food",                new DateTime(2017, 2, 10, 0, 0));
-      Transaction t3 = new Expense(120, "Massage", "Health",            new DateTime(2017, 2, 14, 0, 0));
-      Transaction t4 = new Expense(120, "Massage", "Health",            new DateTime(2017, 2, 15, 0, 0));
-      Transaction t5 = new Expense(100, "Trader Joe's", "Food at home", new DateTime(2017, 3, 1, 0, 0));
-      Transaction t6 = new Income(4000, "Salary", "Salary",             new DateTime(2017, 3, 1, 0, 0));
-      Transaction t7 = new Expense(10, "Amazon", "Nails",               new DateTime(2017, 3, 9, 0, 0));
-      Transaction t8 = new Expense(10, "Amazon", "Nails",               new DateTime(2017, 3, 10, 0, 0));
+      Transaction[] transactions = new Transaction[]
+         {new Income(4000, "Salary", "Salary",             new DateTime(2017, 2, 1, 0, 0))  //0
+         ,new Expense(450, "Lunch", "Food",                new DateTime(2017, 2, 10, 0, 0)) //1
+         ,new Expense(120, "Massage", "Health",            new DateTime(2017, 2, 14, 0, 0)) //2
+         ,new Expense(120, "Massage", "Health",            new DateTime(2017, 2, 15, 0, 0)) //3
+         ,new Expense(100, "Trader Joe's", "Food at home", new DateTime(2017, 3, 1, 0, 0))  //4
+         ,new Income(4000, "Salary", "Salary",             new DateTime(2017, 3, 1, 0, 0))  //5
+         ,new Expense(10, "Amazon", "Nails",               new DateTime(2017, 3, 9, 0, 0))  //6
+         ,new Expense(10, "Amazon", "Nails",               new DateTime(2017, 3, 10, 0, 0)) //7
+         };
       ExpenseBook book = new ExpenseBook();
-      book.addTransaction(t1);
-      book.addTransaction(t2);
-      book.addTransaction(t3);
-      book.addTransaction(t4);
-      book.addTransaction(t5);
-      book.addTransaction(t6);
-      book.addTransaction(t7);
-      book.addTransaction(t8);
+      for (Transaction t : transactions) {
+         book.addTransaction(t);
+      }
 
       DateTime from = new DateTime(2017, 2, 15, 0, 0);
       DateTime to = new DateTime(2017, 3, 9, 0, 0);
       TransactionCondition inPeriodCondition = new TransactionInInclusivePeriodCondition(from, to);
       Collection<Transaction> inPeriod = book.getTransactionsForCondition(inPeriodCondition);
-      assert (inPeriod.contains(t4));
-      assert (inPeriod.contains(t5));
-      assert (inPeriod.contains(t6));
-      assert (inPeriod.contains(t7));
+      assert (inPeriod.contains(transactions[3]));
+      assert (inPeriod.contains(transactions[4]));
+      assert (inPeriod.contains(transactions[5]));
+      assert (inPeriod.contains(transactions[6]));
       assertEquals(4, inPeriod.size());
    }
 
